0d64f9d2e2ec2c4c4438bdafdc8fd449335bfde7
Basko-Vladimir <1989bvg@gmail.com>
2020-06-17 21:57:37 +0300

    Footer css modules were remake at scss modules

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Footer.module.css";
+import styles from "./Footer.module.scss";
 import HeaderBlock from "../common/HeaderBlock/HeaderBlock";
 import Fade from 'react-reveal/Fade';
 import {faVk, faLinkedinIn, faTelegram, faGithub} from '@fortawesome/fontawesome-free-brands';
